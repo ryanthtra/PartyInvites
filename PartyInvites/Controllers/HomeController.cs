4d9e16387a6cfc5cd69658dc236f3948dd71abec
[ThinkPad]Ryan Transfiguracion <rtransfig@gmail.com>
2017-08-13 18:35:32 -0700

    Initial commit.

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,35 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Linq;
 
 namespace PartyInvites.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    public ViewResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
-        }
+      int hour = DateTime.Now.Hour;
+      ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+      return View("MyView");
     }
+
+    [HttpGet]
+    public ViewResult RsvpForm()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ViewResult RsvpForm(GuestResponse guestResponse)
+    {
+      if (ModelState.IsValid)
+      {
+        Repository.AddResponse(guestResponse);
+        return View("Thanks", guestResponse);
+      }
+      else
+      {
+        // There is a validation error
+        return View();
+      }
+    }
+
+    public ViewResult ListResponses()
+    {
+      return View(Repository.Responses.Where(r => r.WillAttend == true));
+    }
+  }
 }
